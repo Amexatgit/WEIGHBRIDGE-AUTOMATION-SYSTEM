b41f5743909666d875f0273471fa902ad4c34be5
Amey <professionalamex@gmail.com>
2025-07-10 15:28:15 +0000

    First draft of opencv

--- vehicle_webbridge/main.cpp
+++ vehicle_webbridge/main.cpp
@@ -3,4 +3,16 @@
 #include <mysql_connection.h>
 #include <mysql_driver.h>
 
+#include <opencv2/opencv.hpp>
+
 #include <iostream>
+
+
+int main() {
+    // im building this code for opencv 
+    // im reffering a youtube toutorial about opencv to learn how things work 
+    cv::Mat image(200, 400, CV_8UC3, cv::Scalar(0, 255, 0));
+    cv::putText(image, "OpenCV OK", {50, 100}, cv::FONT_HERSHEY_SIMPLEX, 1, {0,0,0}, 2);
+    cv::imwrite("test_opencv.jpg", image);
+    std::cout << "Image saved using OpenCV.\n";
+}

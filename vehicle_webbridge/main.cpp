d21a6102d81eddddda5e89a8224c2bf52b3568e5
Amey <professionalamex@gmail.com>
2025-07-11 15:21:30 +0000

    Final draft slected ater testing

--- vehicle_webbridge/main.cpp
+++ vehicle_webbridge/main.cpp
@@ -9,10 +9,12 @@
 
 
 int main() {
-    // im building this code for opencv 
-    // im reffering a youtube toutorial about opencv to learn how things work 
+    
     cv::Mat image(200, 400, CV_8UC3, cv::Scalar(0, 255, 0));
     cv::putText(image, "OpenCV OK", {50, 100}, cv::FONT_HERSHEY_SIMPLEX, 1, {0,0,0}, 2);
     cv::imwrite("test_opencv.jpg", image);
     std::cout << "Image saved using OpenCV.\n";
+
+    // Tested this code in different Codespace working fine 
+    // Finalizing This draft
 }

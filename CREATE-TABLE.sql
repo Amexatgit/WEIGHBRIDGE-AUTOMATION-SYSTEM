8b5b8e3f7171bd4250da00d73fe5a2cffb2a4943
Amey <professionalamex@gmail.com>
2026-02-06 15:32:32 +0530

    Delete CREATE-TABLE.sql

--- CREATE-TABLE.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE vehicle_entries (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    vehicle_number VARCHAR(20),
-    weight_in_kg FLOAT,
-    entry_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    exit_time DATETIME
-);
-
